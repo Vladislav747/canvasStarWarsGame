7a42927e4abc96d113b98aa8e9ea68dc6485750b
Кудряков Владислав <vladislavis@intersvyaz.net>
2021-02-23 01:41:00 +0500

    a

--- script.js
+++ script.js
@@ -8,12 +8,28 @@ const keys = [];
 const player = {
 	x: 0,
 	y: 0,
-	width: 0, // to be calculated
-	height: 0, // to be calculated
+	width: 40, // to be calculated
+	height: 50, // to be calculated
 	frameX: 0,
 	frameY: 0,
-	speed: 9,
+	speed: 9, // Скорость движения
 	moving: false
 };
 
 const playerSprite = new Image();
+playerSprite.src = "assets/ironman.png";
+
+const background = new Image();
+background.src = "assets/background.png"
+
+function drawSprite(img, sX, sY, sW,sH, dX, dY, dW, dH){
+	ctx.drawImage(img, sX, sY, sW,sH, dX, dY, dW, dH);
+}
+
+function animate(){
+	ctx.clearRect(0, 0, canvas.width, canvas.height);    
+	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+	drawSprite(playerSprite, 0, 0, player.width, player.height, 0, 0, player.width, player.height)
+	requestAnimationFrame(animate);
+}
+ animate(); 
