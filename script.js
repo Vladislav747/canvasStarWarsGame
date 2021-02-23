dfb582f780be98ddbe1c1af3ff49d4e14e22f48a
Кудряков Владислав <vladislavis@intersvyaz.net>
2021-02-23 15:23:10 +0500

    Добавил fps

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const keys = [];
 const player = {
 	x: 200,
 	y: 200,
-	width: 38, // to be calculated
+	width: 34, // to be calculated
 	height: 48, // to be calculated
 	frameX: 0,
 	frameY: 0,
@@ -26,22 +26,26 @@ function drawSprite(img, sX, sY, sW,sH, dX, dY, dW, dH){
 	ctx.drawImage(img, sX, sY, sW,sH, dX, dY, dW, dH);
 }
 
-function animate(){
-	ctx.clearRect(0, 0, canvas.width, canvas.height);    
-	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-	drawSprite(playerSprite, player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y, player.width, player.height)
-	movePlayer();
-	requestAnimationFrame(animate);
-}
- animate();
+// function animate(){
+// 	ctx.clearRect(0, 0, canvas.width, canvas.height);    
+// 	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+// 	drawSprite(playerSprite, player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y, player.width, player.height)
+// 	movePlayer();
+// 	handlePlayerFrame();
+// 	requestAnimationFrame(animate);
+// }
+//  animate();
 
  window.addEventListener("keydown", function(e){
 	 //Классное назначение ключа - потому что удобно проверять через if потом ключи
 	 //ArrowUp, ArrowLeft
 	 keys[e.key] = true;
+	 player.moving = true;
+	 
  });
  window.addEventListener("keyup", function(e){
 	 delete keys[e.key];
+	 player.moving = false;
  });
 
  /**
@@ -51,19 +55,56 @@ function animate(){
 	 if(keys["ArrowUp"] && player.y > 100 ){
 		 player.y -= player.speed;
 		 player.frameY = 3;
+		 player.moving = true;
 	 }
 	 if(keys["ArrowDown"] && player.y< 430){
 		player.y += player.speed;
 		player.frameY = 0;
+		player.moving = true;
 	}
 
 	if(keys["ArrowRight"] && player.x< 730){
 		player.x += player.speed;
 		player.frameY = 2;
+		player.moving = true;
 	}
 
 	if(keys["ArrowLeft"] && player.x> 100){
 		player.x -= player.speed;
 		player.frameY = 1;
+		player.moving = true;
 	}
   }
+
+  function handlePlayerFrame() {
+	if(player.frameX < 3 && player.moving) player.frameX++;
+	else player.frameX = 0;
+}
+
+let fps, fpsInterval, startTime, now, then, elapsed;
+
+function startAnimating(fps) {
+	fpsInterval = 1000/fps;
+	then = Date.now();
+	startTime = then;
+	animate();
+}
+
+function animate() {
+	requestAnimationFrame(animate);
+	now = Date.now();
+	elapsed = now - then;
+	//Если время бежит слишком быстро то мы его уменьшаем
+	if (elapsed > fpsInterval) {
+		then = now - (elapsed % fpsInterval);
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+		drawSprite(playerSprite, player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y, player.width, player.height);
+		movePlayer();
+		handlePlayerFrame();
+	}
+}
+
+startAnimating(15);  
+
+
